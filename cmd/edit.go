cc4cf58ededcae1bcd8cb5fc75ffb3e27ee3f754
Divi <divyansh@hasura.io>
2021-06-21 03:56:42 +0530

    Add edit command

---
+++ cmd/edit.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+    "context"
+	"fmt"
+    "strings"
+
+	"github.com/spf13/cobra"
+    "github.com/hasura/go-graphql-client"
+)
+
+var editCmd = &cobra.Command{
+	Use:   "edit --id <id> <body>",
+	Short: "",
+	Run: func(cmd *cobra.Command, args []string) {
+        if len(args) == 0 {
+            cmd.Help()
+            return
+        }
+        client := graphql.NewClient("http://localhost:8080/v1/graphql", nil)
+        editTodo(client, strings.Join(args, " "))
+	},
+}
+
+var id int
+
+var mutateEdit struct {
+    UpdateTodoQuery struct {
+        ID graphql.Int
+    } `graphql:"update_todos_by_pk(pk_columns: {id: $id}, _set: {body: $body})"`
+}
+
+func editTodo(client *graphql.Client, body string) {
+    err := client.Mutate(context.Background(), &mutateEdit,
+        map[string]interface{}{
+            "id": graphql.Int(id),
+            "body": graphql.String(body),
+        },
+    )
+
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Println("Successfully edited todo!")
+}
+
+func init() {
+	rootCmd.AddCommand(editCmd)
+
+    editCmd.Flags().IntVar(&id, "id", 0, "ID of the todo to edit")
+    editCmd.MarkFlagRequired("id")
+}
